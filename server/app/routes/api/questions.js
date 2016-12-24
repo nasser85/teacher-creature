5774b5fcafc882f16b2c9bf85651f1c790717e45
Nasser Abouelazm <nasser.abouelazm@gmail.com>
2016-12-24 00:25:00 -0600

    basic back end routes

---
+++ server/app/routes/api/questions.js
@@ -0,0 +1,9 @@
+'use strict';
+var router = require('express').Router();
+module.exports = router;
+var _ = require('lodash');
+
+
+router.use(function (req, res) {
+    res.status(404).end();
+});
---
+++ server/app/routes/api/users.js
@@ -0,0 +1,11 @@
+'use strict';
+var router = require('express').Router();
+module.exports = router;
+var _ = require('lodash');
+
+
+
+
+router.use(function (req, res) {
+    res.status(404).end();
+});
