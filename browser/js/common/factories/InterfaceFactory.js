879766d8ad85a63df43324c21edd968d97b834e8
Nasser Abouelazm <nasser.abouelazm@gmail.com>
2017-01-29 16:09:00 -0500

    Fixed up introduction video design for home page of site

--- browser/js/common/factories/InterfaceFactory.js
+++ browser/js/common/factories/InterfaceFactory.js
@@ -33,7 +33,42 @@ app.factory('InterfaceFactory', function() {
 			            icon: "markunread_mailbox",
 			            link: "students({classId: currentClass.id})"
 			        }
-			    ];
+				];
+	}
+
+	interfaceFactory.siteFeatures = function() {
+		return [
+			        {
+			            label: "Build",
+			            icon: "supervisor_account",
+			            link: "students({classId: currentClass.id})"
+			        },
+			        {
+			            label: "Assignments",
+			            icon: "note_add",
+			            link: "students({classId: currentClass.id})"
+			        },
+			        {
+			            label: "Exams",
+			            icon: "spellcheck",
+			            link: "students({classId: currentClass.id})"
+			        },
+			        {
+			            label: "Resources",
+			            icon: "https",
+			            link: "students({classId: currentClass.id})"
+			        },
+			        {
+			            label: "All Classes",
+			            icon: "recent_actors",
+			            link: "students({classId: currentClass.id})"
+			        },
+			        {
+			            label: "Mailbox",
+			            icon: "markunread_mailbox",
+			            link: "students({classId: currentClass.id})"
+			        }
+				];
 	}
 
 
